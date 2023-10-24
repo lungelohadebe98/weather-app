e9ecd2413cc1ed44da03dd9e1e341f93a1b88013
lungelohadebe98 <118820935+lungelohadebe98@users.noreply.github.com>
2023-10-24 12:49:00 +0200

    added api

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("HELOO");
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innnerhtml = response.condition.description;
+  iconElement.innerhtml = response.data.condition.icon;
+}
+
+let city = "Johannesburg";
+let apiKey = "88td453618166250a7dc33aob396fe0b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);

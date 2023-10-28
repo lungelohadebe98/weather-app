26af42aaca0a8da5b5acaa6f2e47c25e80a88722
lungelohadebe98 <118820935+lungelohadebe98@users.noreply.github.com>
2023-10-28 02:11:49 +0200

    Changed  display

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function displayTemperature(response) {
   console.log(response.data);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+}
+function searchCity(city) {
+  let apiKey = "88td453618166250a7dc33aob396fe0b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let iconElement = document.querySelector("#icon");
-
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innnerhtml = response.condition.description;
-  iconElement.innerhtml = response.data.condition.icon;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let city = "Johannesburg";
-let apiKey = "88td453618166250a7dc33aob396fe0b";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
-axios.get(apiUrl).then(displayTemperature);
+searchCity("Johannesburg");
